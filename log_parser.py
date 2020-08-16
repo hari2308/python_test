f2115ad0152637e2766d9c704e01ef3a7ed0fc8d
Hari Krishna Vakalapudi <vhk2308@gmail.com>
2020-08-16 12:01:36 +0530

    made changes according to PEP8

--- log_parser.py
+++ log_parser.py
@@ -22,7 +22,8 @@ class LogParser():
         self.file = files
         self.reg = reg
         self.key_list = keys_list
-        self.host_list,self.request_list, self.status_list,self.dict_list = self._parsing()
+        self.host_list, self.request_list, \
+        self.status_list, self.dict_list = self._parsing()
         self.unreq_count, self.per_suc, self.per_unsuc = self._calculation()
 
     def _parsing(self):
@@ -32,21 +33,21 @@ class LogParser():
         status
         """
         host_list = []
-        request_list = [] 
-        status_list = [] 
+        request_list = []
+        status_list = []
         dict_list = []
 
-        with open(self.file, "r", encoding='utf-8', errors='ignore') as f:
-            log = f.read()
-            my_list = re.findall(self.reg,log)
+        with open(self.file, "r", encoding='utf-8', errors='ignore') as data:
+            log = data.read()
+            my_list = re.findall(self.reg, log)
             for data in my_list:
                 host_list.append(data[0])
                 request_list.append(data[1])
                 status_list.append(data[2])
-                dicts = dict(map(lambda key, values: (key,values), self.key_list, data))
+                dicts = dict(map(lambda key, values: (key, values), self.key_list, data))
                 dict_list.append(dicts)
 
-        return host_list, request_list, status_list, dict_list        
+        return host_list, request_list, status_list, dict_list
 
     def req_page(self):
         """
@@ -55,12 +56,12 @@ class LogParser():
         request_count = Counter(self.request_list)
         print("\nTop 10 Requested pages along with count:\n")
         for key, value in request_count.most_common(10):
-            print(f'Request page - "{key}",  Hits - {value}')      
+            print(f'Request page - "{key}",  Hits - {value}')
 
     def _calculation(self):
         """
         Calculates percentage of successful and unsuccessful status
-        and returns the unsuccessful requests as well 
+        and returns the unsuccessful requests as well
         """
         status_count = Counter(self.status_list)
         total = sum(map(int, status_count.values()))
@@ -72,7 +73,7 @@ class LogParser():
                 count = count+1
         unreq_count = Counter(unlist)
         if total != 0:
-            per_unsuc = count/total 
+            per_unsuc = count/total
             per_suc = 1-(count/total)
         else:
             per_unsuc = 0
@@ -84,19 +85,19 @@ class LogParser():
         """
         prints the successful percents of requests
         """
-        
+
         print(f'\nPercentage of successful requests - {self.per_suc *100}')
 
     def unsuccessful_per(self):
         """
         prints the successful percents of requests
         """
-        
+
         print(f'\nPercentage of unsuccessful requests - {self.per_unsuc *100}')
 
     def mosthost(self):
         """
-        prints the top hosts 
+        prints the top hosts
         """
         host_count = Counter(self.host_list)
         print("\nTop 10 Host along with count:\n")
@@ -128,21 +129,29 @@ class LogParser():
             print(f"\nSites visited by host '{key[0]}'\n")
             for site, count in cov.most_common(5):
                 print(f"site - '{site}' and no.of visits- {count}")
-               
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='HTTP Log parser')
-    parser.add_argument("-l","--logfile", help="logfile input", required=True)
-    parser.add_argument("-a","--all", help="prints all data", default=True)
-    parser.add_argument("-rq","--most_req", help="Mosted requested pages and count",action='store_true')
-    parser.add_argument("-ps","--per_suc", help="percentage of Successful requests ",action='store_true')
-    parser.add_argument("-pu","--per_unsuc", help="percentage of Unsuccessful requests",action='store_true')
-    parser.add_argument("-mu","--most_unreq", help="Mosted unsuccessful requests",action='store_true')
-    parser.add_argument("-ip","--host", help="Most requested by host and there count",action='store_true')
-    parser.add_argument("-tp","--top",help="prints the  top 10 data",action='store_true')
+    parser.add_argument("-l", "--logfile", help="logfile input", required=True)
+    parser.add_argument("-a", "--all", help="prints all data", default=True)
+    parser.add_argument("-rq", "--most_req", help="Mosted requested pages and count", \
+                        action='store_true')
+    parser.add_argument("-ps", "--per_suc", help="percentage of Successful requests", \
+                        action='store_true')
+    parser.add_argument("-pu", "--per_unsuc", help="percentage of Unsuccessful requests",\
+                        action='store_true')
+    parser.add_argument("-mu", "--most_unreq", help="Mosted unsuccessful requests", \
+                        action='store_true')
+    parser.add_argument("-ip", "--host", \
+                        help="Most requested by host and there count",\
+                        action='store_true')
+    parser.add_argument("-tp", "--top", help="prints the  top 10 data",\
+                        action='store_true')
     args = parser.parse_args()
 
-    arg = [args.most_req,args.per_suc,args.per_unsuc,args.most_unreq,args.host,args.top]
+    arg = [args.most_req, args.per_suc, args.per_unsuc, \
+           args.most_unreq, args.host, args.top]
 
     if args.all == any(arg):
         args.all = False
